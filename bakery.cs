67fd23532a9432e782d37f8ab882e726ade3ca05
Carrie Schmid <carriepederson7@gmail.com>
2019-09-27 10:48:59 -0700

    Add function to calculate loaf without special.

--- bakery.cs
+++ bakery.cs
@@ -1,53 +1,47 @@
-//
-// When the user runs the application, they should receive a prompt with a welcome message along with the cost for both Bread and Pastry.
-//
-// A user should be able to specify how many loaves of Bread and how many Pastrys they'd like.
-//
-// The application will return the total cost of the order.
-//
-// Pierre offers the following deals:
-//
-// Bread: Buy 2, get 1 free. A single loaf costs $5.
-// Pastry: Buy 1 for \$2 or 3 for $5.
 
 namespace BakeryStore
 {
   public class Bread
   {
-    public int Loaf {get; set;}
-    public int BreadCost {get; set;}
+    // public string Loaf {get; set;}
+    public int LoafCost {get; set;}
+    public int BreadCount {get; set;}
 
     public Bread()
     {
-      Loaf = 5;
-      BreadCost = 0;
+      // Loaf = "loaf";
+      LoafCost = 0;
+      BreadCount = 0;
     }
 
-    public int CalculateBread()
+    public int AddLoaf(int loafCount)
     {
-      BreadCost = BreadCost + Loaf;
-      return BreadCost;
+      LoafCost = loafCount * 5;
+      BreadCount ++;
+      return LoafCost;
     }
 
   }
 
-  public class Pastry
-  {
-    public int Muffin {get; set;}
-    public int PastryCost {get; set;}
-
-    public Pastry()
-    {
-      Muffin = 3;
-      PastryCost = 0;
-    }
-
-    public int CalculatePastry()
-    {
-      PastryCost = PastryCost + Muffin;
-      return PastryCost;
-    }
-
-  }
+  // public class Pastry
+  // {
+  //   public int Muffin {get; set;}
+  //   public int PastryCost {get; set;}
+  //   public int PastryCount {get; set;}
+  //
+  //   public Pastry(string breadChoice)
+  //   {
+  //     MuffinCost = 2;
+  //     PastryCount = 0;
+  //   }
+  //
+  //   public int CalculatePastry()
+  //   {
+  //     PastryCost = PastryCost + Muffin;
+  //     PastryCount ++;
+  //     return PastryCost;
+  //   }
+  //
+  // }
 
 }
--- program.cs
+++ program.cs
@@ -9,6 +9,28 @@ namespace BakeryStore
   {
     public static void Main()
     {
+      Bread newBread = new Bread();
+
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We sell muffins for $2 and loaves of bread for $5.");
+      Console.WriteLine("Loaf Special: Buy 2, get 1 free.");
+      Console.WriteLine("Muffin Special: Buy 2, get third muffin for $1.");
+
+      Console.WriteLine("What would you like to buy? loaf/muffin");
+      string breadChoice = Console.ReadLine();
+      if(breadChoice == "loaf" || breadChoice == "Loaf")
+      {
+        Console.WriteLine("How many loaves would you like to buy?");
+        string stringLoafCount = Console.ReadLine();
+        int loafCount = int.Parse(stringLoafCount);
+
+        Console.WriteLine(loafCount);
+
+
+        Console.WriteLine(newBread.AddLoaf(loafCount));
+        Console.WriteLine(newBread.BreadCount);
+      }
+
 
     }
 
